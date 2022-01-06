fbbc941360bd62d068e4d08f20d12299023d0657
if20b204 <if20b204@technikum-wien.at>
2022-01-06 21:37:27 +0100

    Final Commit ws22

--- LungenSimulatorApp/Assets/Scripts_Selfwritten/DropDownScript.cs
+++ LungenSimulatorApp/Assets/Scripts_Selfwritten/DropDownScript.cs
@@ -11,10 +11,12 @@ public class DropDownScript : MonoBehaviour
         settingsLabel, settingsText1;
     public RawImage backgroundChanger, fhLogo;
     public GameObject original;
-    public GameObject merge;
+    public GameObject animationWithAir;
     public GameObject splitup;
     public GameObject animate;
     public GameObject functions;
+
+    public bool objectActive = false;
 
     private Vector2 fingerDown;
     private Vector2 fingerUp;
@@ -27,56 +29,61 @@ public class DropDownScript : MonoBehaviour
         switch (selected)
         {
             case 0: //Original no object
+                objectActive = true;
                 fontSizeChoser.gameObject.SetActive(false);
                 settingsLabel.gameObject.SetActive(false);
                 settingsText1.gameObject.SetActive(false);
                 setUp_Tutorials(false);
-                merge.SetActive(false);
+                animationWithAir.SetActive(false);
                 original.SetActive(true);
                 splitup.SetActive(false);
                 animate.SetActive(false);
                 functions.SetActive(false);
                 break;
             case 1: //Split Up object
+                objectActive = true;
                 fontSizeChoser.gameObject.SetActive(false);
                 settingsLabel.gameObject.SetActive(false);
                 settingsText1.gameObject.SetActive(false);
                 setUp_Tutorials(false);
                 original.SetActive(false);
-                merge.SetActive(false);
+                animationWithAir.SetActive(false);
                 splitup.SetActive(true);
                 animate.SetActive(false);
                 functions.SetActive(false);
                 break;
             case 2: //Merge object
+                objectActive = true;
                 fontSizeChoser.gameObject.SetActive(false);
                 settingsLabel.gameObject.SetActive(false);
                 settingsText1.gameObject.SetActive(false);
                 original.SetActive(false);
                 setUp_Tutorials(false);
-                merge.SetActive(true);
+                animationWithAir.SetActive(true);
                 splitup.SetActive(false);
                 animate.SetActive(false);
                 functions.SetActive(false);
                 break;
             case 3: //Animate object
+                objectActive = true;
                 fontSizeChoser.gameObject.SetActive(false);
                 original.SetActive(false);
                 settingsLabel.gameObject.SetActive(false);
                 settingsText1.gameObject.SetActive(false);
                 setUp_Tutorials(false);
-                merge.SetActive(false);
+                animationWithAir.SetActive(false);
                 splitup.SetActive(false);
                 animate.SetActive(true);
                 functions.SetActive(false);
                 break;
             case 4: //Schow Functions of the object
+                objectActive = true;
                 fontSizeChoser.gameObject.SetActive(false);
                 original.SetActive(false);
                 settingsLabel.gameObject.SetActive(false);
                 settingsText1.gameObject.SetActive(false);
                 setUp_Tutorials(false);
-                merge.SetActive(false);
+                animationWithAir.SetActive(false);
                 splitup.SetActive(false);
                 animate.SetActive(false);
                 functions.SetActive(true);
@@ -87,8 +94,10 @@ public class DropDownScript : MonoBehaviour
                 settingsText1.gameObject.SetActive(false);
                 setUp_Tutorials(true);
                 removeAllLunSimObjects();
+                objectActive = false;
                 break;
             case 6: //Settings
+                objectActive = false;
                 setUp_Tutorials(false);
                 backgroundChanger.gameObject.SetActive(true);
                 fhLogo.gameObject.SetActive(true);
@@ -117,16 +126,6 @@ public class DropDownScript : MonoBehaviour
                 fingerUp = touch.position;
                 fingerDown = touch.position;
             }
-
-            //Detects Swipe while finger is still moving
-            /*if (touch.phase == TouchPhase.Moved)
-            {
-                if (!detectSwipeOnlyAfterRelease)
-                {
-                    fingerDown = touch.position;
-                    checkSwipe();
-                }
-            }*/
 
             //Detects swipe after finger is released
             if (touch.phase == TouchPhase.Ended)
@@ -189,22 +188,48 @@ public class DropDownScript : MonoBehaviour
     //////////////////////////////////CALLBACK FUNCTIONS/////////////////////////////
     void OnSwipeUp()
     {
-        //Maybe put in sth later
+        if (objectActive == true)
+        {
+            original.transform.localScale += new Vector3(1f, 1f, 1f);
+            original.transform.position = new Vector3(original.transform.position.x - 0.8f, original.transform.position.y - 0.15f, original.transform.position.z - 0.2f);
+            animationWithAir.transform.localScale += new Vector3(1f, 1f, 1f);
+            animationWithAir.transform.position = new Vector3(animationWithAir.transform.position.x - 0.8f, animationWithAir.transform.position.y - 0.15f, animationWithAir.transform.position.z - 0.2f);
+            splitup.transform.localScale += new Vector3(1f, 1f, 1f);
+            splitup.transform.position = new Vector3(splitup.transform.position.x - 0.8f, splitup.transform.position.y - 0.15f, splitup.transform.position.z - 0.2f);
+            animate.transform.localScale += new Vector3(1f, 1f, 1f);
+            animate.transform.position = new Vector3(animate.transform.position.x - 0.8f, animate.transform.position.y - 0.15f, animate.transform.position.z - 0.2f);
+            functions.transform.localScale += new Vector3(1f, 1f, 1f);
+            functions.transform.position = new Vector3(functions.transform.position.x - 0.8f, functions.transform.position.y - 0.15f, functions.transform.position.z - 0.2f);
+        }
+
+        
     }
 
     void OnSwipeDown()
     {
-        //Maybe put in sth later
+        if (objectActive == true)
+        {
+            original.transform.localScale -= new Vector3(1f, 1f, 1f);
+            original.transform.position = new Vector3(original.transform.position.x + 0.8f, original.transform.position.y + 0.15f, original.transform.position.z + 0.2f);
+            animationWithAir.transform.localScale -= new Vector3(1f, 1f, 1f);
+            animationWithAir.transform.position = new Vector3(animationWithAir.transform.position.x + 0.8f, animationWithAir.transform.position.y + 0.15f, animationWithAir.transform.position.z + 0.2f);
+            splitup.transform.localScale -= new Vector3(1f, 1f, 1f);
+            splitup.transform.position = new Vector3(splitup.transform.position.x + 0.8f, splitup.transform.position.y + 0.15f, splitup.transform.position.z + 0.2f);
+            animate.transform.localScale -= new Vector3(1f, 1f, 1f);
+            animate.transform.position = new Vector3(animate.transform.position.x + 0.8f, animate.transform.position.y + 0.15f, animate.transform.position.z + 0.2f);
+            functions.transform.localScale -= new Vector3(1f, 1f, 1f);
+            functions.transform.position = new Vector3(functions.transform.position.x + 0.8f, functions.transform.position.y + 0.15f, functions.transform.position.z + 0.2f);
+        }
     }
 
     void OnSwipeLeft()
     {
-        whatToDo.value += + 1;
+        whatToDo.value += 1;
     }
 
     void OnSwipeRight()
     {
-        whatToDo.value -= whatToDo.value;
+        whatToDo.value -= 1;
     }
 
     void setUp_Tutorials(bool b)
@@ -222,7 +247,7 @@ public class DropDownScript : MonoBehaviour
     void removeAllLunSimObjects()
     {
         original.SetActive(false);
-        merge.SetActive(false);
+        animationWithAir.SetActive(false);
         splitup.SetActive(false);
         animate.SetActive(false);
         functions.SetActive(false);
